9e7473f8e977991ebc3f9f76bff13b4317951d2b
Frank Denis <github@pureftpd.org>
2018-05-03 09:32:26 +0200

    Initial import

---
+++ src/randen.h
@@ -0,0 +1,26 @@
+#ifndef randen_H
+#define randen_H
+
+#include <stdint.h>
+
+#define RANDEN_STATE_BYTES 256
+#define RANDEN_SEED_BYTES  (RANDEN_STATE_BYTES - 16)
+
+#ifndef CRYPTO_ALIGN
+# if defined(__INTEL_COMPILER) || defined(_MSC_VER)
+#  define CRYPTO_ALIGN(x) __declspec(align(x))
+# else
+#  define CRYPTO_ALIGN(x) __attribute__ ((aligned(x)))
+# endif
+#endif
+
+typedef struct RandenState_ {
+    CRYPTO_ALIGN(32) uint8_t state[RANDEN_STATE_BYTES];
+    uint64_t next;
+} RandenState;
+
+void randen_init(RandenState *st, const uint8_t seed[RANDEN_SEED_BYTES]);
+void randen_reseed(RandenState *st, const uint8_t seed[RANDEN_SEED_BYTES]);
+uint8_t randen_generate_byte(RandenState *st);
+
+#endif
